54f282796988511bf7c1fd5c02c8a3fef42903aa
Rupert Broad <rupert.broad@gmail.com>
2018-02-25 17:51:03 +0100

    tfm fmt

--- codebuild/build_ami_ws.tf
+++ codebuild/build_ami_ws.tf
@@ -94,7 +94,7 @@ resource "aws_codebuild_project" "build_ami_ws" {
     location  = "${var.pckr_source}"
 
     auth {
-      "type" = "OAUTH"
+      "type"     = "OAUTH"
       "resource" = "${var.oauth_token}"
     }
   }
